b0460fe8d9d729d8c2df6b01c48f62f13a7ec710
Shan Moorthy <github@shan.moorthy.com.au>
2020-08-20 10:57:14 +1000

    Init

---
+++ Formula/dce.rb
@@ -0,0 +1,18 @@
+class Dce < Formula
+    desc "Disposable Cloud Environment CLI"
+    homepage "https://github.com/Optum/dce-cli"
+    version "0.5.0"
+    bottle :unneeded
+  
+    if OS.mac?
+        url "https://github.com/Optum/dce-cli/releases/download/v0.5.0/dce_darwin_amd64.zip"
+        sha256 "3056abbcd1a7fc55bc149aa71dd763dbd007a7433487cd66293c32b393419e53"
+    elsif OS.linux?
+        if Hardware::CPU.arm?
+            if Hardware::CPU.is_64_bit?
+                url "https://github.com/Optum/dce-cli/releases/download/v0.5.0/dce_linux_amd64.zip"
+                sha256 "cb140c743373e28a6c1bd4ba3fe1b81a7431dd538e1ad430fede3c1aff4508db"
+            end
+        end
+    end
+end
